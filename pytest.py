ca6ee33d23545ec5c5fbffd787677942b2991ea4
keyuls <keyul@asu.edu>
2017-07-09 22:22:49 -0700

    test case for image upload

--- pytest.py
+++ pytest.py
@@ -1,16 +1,16 @@
 
 import unittest
 import DataIncubator
+import io
 
 class DiTestCase(unittest.TestCase):
 
     def setUp(self):
         self.app = DataIncubator.app.test_client()
 
-    def test_empty_path(self):
-        response = self.app.get('/')
-        print(response)
-        self.assertEqual(response.status_code, 200)
+    def test_image_upload(self):
+        response = self.app.post('/img/', content_type='multipart/form-data',data={'picture': (io.BytesIO(b'test image file'), 'testimage.jpeg')})
+        self.assertEqual(response.data,b'test image file')
 
 if __name__ == '__main__':
     unittest.main()
