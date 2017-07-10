f7f19a59e28156de2d6c15a875240d4f7c362e6a
Keyul <keyul.shah1@gmail.com>
2017-07-10 12:32:34 -0700

    comments for test file

--- pytest.py
+++ pytest.py
@@ -4,10 +4,11 @@ import DataIncubator
 import io
 
 class DiTestCase(unittest.TestCase):
-
+    # setup for unit testing
     def setUp(self):
         self.app = DataIncubator.app.test_client()
-
+        
+    # test case for POST request for image upload
     def test_image_upload(self):
         response = self.app.post('/img/', content_type='multipart/form-data',data={'picture': (io.BytesIO(b'test image file'), 'testimage.jpeg')})
         self.assertEqual(response.data,b'test image file')
