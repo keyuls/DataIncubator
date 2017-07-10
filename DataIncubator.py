0d8c8c90dcc4af502b875f49801c32cd23d997b4
Keyul <keyul.shah1@gmail.com>
2017-07-10 12:29:52 -0700

    Add comments

--- DataIncubator.py
+++ DataIncubator.py
@@ -2,22 +2,28 @@ import os
 from flask import *
 from werkzeug.utils import secure_filename
 
+# Retrieve absolute path of directory 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+# Specify location to save images temporary
 UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/images')
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
+# default getway when script run
 @app.route('/')
 def upload_image():
-    return render_template('index.html', result= 'page display successfully')
+    return render_template('index.html')
 
+# perofm image upload and display 
 @app.route('/img/',methods=['POST'])
 def submit_image():
+    #retrieve file from post request data
     image = request.files['picture']
     filename = secure_filename(image.filename)
+    # save image temporary
     image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    #send image back to user as a response
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
